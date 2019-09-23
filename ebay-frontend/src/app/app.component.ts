552a8614b769e2753917ea4e27129f7dfc058de7
Brayden Cloud <brayden14cloud@gmail.com>
2019-09-22 22:01:08 -0500

    fixed merge conflicts

--- ebay-frontend/src/app/app.component.ts
+++ ebay-frontend/src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/services/auth.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'ebay-frontend';
+export class AppComponent implements OnInit {
+  profile: any;
+  user: any;
+  token: string;
+
+  constructor(private authService: AuthService) { }
+
+  ngOnInit() {
+    this.authService.localAuthSetup();
+
+    this.authService.userProfile$.subscribe((profile) => {
+      this.profile = profile;
+    });
+    this.authService.getUser$().subscribe((user) => this.user = user);
+    this.authService.token$.subscribe((token) => this.token = token);
+  }
 }
