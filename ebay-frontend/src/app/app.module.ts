f96340c424a5a8115d8bc61e5b294eec485dfbca
Casey Kinnamon <caseykinnamon@my.unt.edu>
2019-09-26 14:59:19 -0500

    fixed spacing issue

--- ebay-frontend/src/app/app.module.ts
+++ ebay-frontend/src/app/app.module.ts
@@ -1,12 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {MatToolbarModule} from '@angular/material/toolbar';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { HomeComponent } from './home/home.component';
 import { CallbackComponent } from './callback/callback.component';
-import {MatButtonModule} from '@angular/material/button'; 
+import { MatButtonModule } from '@angular/material/button'; 
 @NgModule({
   declarations: [
     AppComponent,
