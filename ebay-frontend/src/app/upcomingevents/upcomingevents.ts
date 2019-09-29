e3e2f50a31356018fd52cb0f22c3b482b470c611
joe-niu <joeniu@my.unt.edu>
2019-09-29 18:23:33 -0500

    Adding Upcoming Events folder with  3 files included

--- ebay-frontend/src/app/upcomingevents/upcomingevents.ts
+++ ebay-frontend/src/app/upcomingevents/upcomingevents.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-    selector: 'home',
+    selector: 'upcomingevents',
     templateUrl: './upcomingevents.component.html',
     styleUrls: ['./upcomingevents.component.scss']
   })
