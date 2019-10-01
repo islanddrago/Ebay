39efd6a93e94d1fc237330bc73847e463d484aa2
Casey Kinnamon <caseykinnamon@my.unt.edu>
2019-10-01 13:03:37 -0500

    Navbar finished

--- ebay-frontend/src/app/app.module.ts
+++ ebay-frontend/src/app/app.module.ts
@@ -7,18 +7,30 @@ import { NavbarComponent } from './navbar/navbar.component';
 import { HomeComponent } from './home/home.component';
 import { CallbackComponent } from './callback/callback.component';
 import { MatButtonModule } from '@angular/material/button'; 
+import { MatIconModule } from '@angular/material/icon';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatListModule } from '@angular/material/list';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MainNavComponent } from './main-nav/main-nav.component';
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     HomeComponent,
-    CallbackComponent
+    CallbackComponent,
+    MainNavComponent,
   ],
   imports: [
     BrowserModule,
     MatToolbarModule,
     AppRoutingModule,
     MatButtonModule,
+    MatIconModule,
+    MatSidenavModule,
+    BrowserAnimationsModule,
+    LayoutModule,
+    MatListModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
