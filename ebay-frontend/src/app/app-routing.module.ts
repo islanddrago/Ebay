ae0f1f5f246ebc64c908dc6034c141cab629054b
Casey Kinnamon <caseykinnamon@my.unt.edu>
2019-11-14 00:44:46 -0600

    fixed format

--- ebay-frontend/src/app/app-routing.module.ts
+++ ebay-frontend/src/app/app-routing.module.ts
@@ -1,14 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { HomeComponent } from './home/home.component';
 import { CallbackComponent } from './callback/callback.component';
 import { SettingsScreenComponent } from './settings-screen/settings-screen.component';
 import { UpcomingEventsScreenComponent } from './upcoming-events-screen/upcoming-events-screen.component';
 import { PastEventsScreenComponent } from './past-events-screen/past-events-screen.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
 
 
 const routes: Routes = [
-  { path: '', component: HomeComponent, pathMatch: 'full' },
+  { path: '', component: LandingPageComponent, pathMatch: 'full' },
   { path: 'upcoming-events', component: UpcomingEventsScreenComponent, pathMatch: 'full' },
   { path: 'past-events', component: PastEventsScreenComponent, pathMatch: 'full' },
   { path: 'settings', component: SettingsScreenComponent, pathMatch: 'full' },
