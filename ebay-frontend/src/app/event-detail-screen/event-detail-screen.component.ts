4343fb779b9fa06ce7789c56d98ce1cd1b56941f
Gabriel Castro <mateocastro33@gmail.com>
2019-11-29 16:54:10 -0600

    created event detail screen component

---
+++ ebay-frontend/src/app/event-detail-screen/event-detail-screen.component.ts
@@ -0,0 +1,44 @@
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'app-event-detail-screen',
+  templateUrl: './event-detail-screen.component.html',
+  styleUrls: ['./event-detail-screen.component.scss']
+})
+export class EventDetailScreenComponent implements OnInit {
+
+  
+
+  event = { // Will be populated in onInit
+    name: "Volleyball",
+    date: "September 12th",
+    startTime: "2:00 PM",
+    endTime: "4:00 PM",
+    hostName: "Gabriel",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.",
+    latitude: 33.214480,
+    longitude: -97.145870,
+    rsvps: [
+      {
+        img: 'assets/beff-jezos.jpg',
+        name: 'Beff Jezos',
+      },
+      {
+        img: 'assets/beff-jezos.jpg',
+        name: 'Steph Jezos',
+      },
+      {
+        img: 'assets/beff-jezos.jpg',
+        name: 'Carl',
+      }
+    ]
+  }
+  
+  mapType = 'roadmap';
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
