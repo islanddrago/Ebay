56d3da8197df2d92980a3ab982d9ce88ad5679ff
Brayden Cloud <brayden14cloud@gmail.com>
2019-12-01 17:07:42 -0600

    changed database

--- ebay-backend/src/constants/db.constants.ts
+++ ebay-backend/src/constants/db.constants.ts
@@ -2,6 +2,6 @@ export const DATABASE = "ebay";
 export const USERS_COLLECTION = "users";
 export const EVENTS_COLLECTION = "events";
 
-// export const CONNECTION_STRING =
-//   "mongodb+srv://ebay:event-bay@cluster0-1iadj.mongodb.net/test?retryWrites=true&w=majority";
-export const CONNECTION_STRING = "mongodb://127.0.0.1";
+export const CONNECTION_STRING =
+  "mongodb+srv://ebay:event-bay@cluster0-1iadj.mongodb.net/test?retryWrites=true&w=majority";
+// export const CONNECTION_STRING = "mongodb://127.0.0.1";
